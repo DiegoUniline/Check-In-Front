c305a5a20be98737d83a6ebffb78707e56cc141c
DiegoUniline <diego.leon@uniline.mx>
2026-01-15 21:54:56 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Importamos el Router
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Usamos el basename para que coincida con tu repositorio de GitHub
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/Check-In-Front">
+    <App />
+  </BrowserRouter>
+);
