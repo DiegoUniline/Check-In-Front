2fcaf89a531a8ccd755ed6e1ffa41885e262b2c3
DiegoUniline <diego.leon@uniline.mx>
2026-01-15 22:16:38 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,24 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Nombre de tu repositorio en GitHub
-  base: '/Check-In-Front/', 
-  
+  base: "/Check-In-Front/",
   server: {
     host: "::",
     port: 8080,
   },
-  
   plugins: [
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
-
   resolve: {
     alias: {
-      // Esto asegura que las importaciones con "@" funcionen siempre
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
   build: {
-    // Esto ayuda a que los archivos generados se organicen mejor
     outDir: "dist",
     assetsDir: "assets",
-    // Evita problemas de carga si se limpia la caché
     emptyOutDir: true,
-  }
+  },
 }));
